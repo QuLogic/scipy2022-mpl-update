fa4580e97390a9a4b4386c5fd94c9eeba8b0959c
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2021-07-15 01:11:56 -0400

    Update documentation slide.

--- docs.py
+++ docs.py
@@ -2,7 +2,7 @@
 Documentation highlights.
 """
 
-from mplslide import FONT, BULLET, new_slide, slide_heading
+from mplslide import FONT, new_slide, slide_heading
 
 
 def docs():
@@ -10,23 +10,31 @@ def docs():
 
     slide_heading(fig, 'Documentation')
 
-    props = dict(fontproperties=FONT, fontsize=56, alpha=0.7,
+    props = dict(fontproperties=FONT, fontsize=48, alpha=0.7,
                  verticalalignment='top')
 
-    fig.text(0.05, 0.8, 'Large and ongoing documentation rewrite', **props)
-    fig.text(0.1, 0.8, f'\n{BULLET} Refreshed homepage', **props)
-    fig.text(0.1, 0.8, f'\n\n{BULLET} 323 Pull Requests in 3.3 alone', **props)
-
-    fig.text(0.05, 0.5, "3.3 What's New?", **props)
-    t = fig.text(0.1, 0.5,
-                 '\nhttps://matplotlib.org/3.3.0/users/whats_new.html',
+    fig.text(0.05, 0.7, "3.4 What's New?", **props)
+    t = fig.text(0.07, 0.7,
+                 '\nhttps://matplotlib.org/3.4.0/users/whats_new.html',
                  **props)
-    t.set_url('https://matplotlib.org/3.3.0/users/whats_new.html')
+    t.set_url('https://matplotlib.org/3.4.0/users/whats_new.html')
 
-    fig.text(0.05, 0.3, 'Cheatsheets! By @rougier', **props)
-    t = fig.text(0.1, 0.3, '\nhttps://github.com/matplotlib/cheatsheets/',
+    fig.text(0.05, 0.5, "Upcoming 3.5 What's New?", **props)
+    t = fig.text(0.07, 0.5,
+                 '\nhttps://matplotlib.org/devdocs/users/whats_new.html',
                  **props)
-    t.set_url('https://github.com/matplotlib/cheatsheets/')
+    t.set_url('https://matplotlib.org/devdocs/users/whats_new.html')
+    fig.text(0.05, 0.5, '\n\nOnce tagged, will be at:', **props)
+    t = fig.text(0.07, 0.5,
+                 '\n\n\nhttps://matplotlib.org/3.5.0/users/whats_new.html',
+                 **props)
+    t.set_url('https://matplotlib.org/3.5.0/users/whats_new.html')
+
+    fig.text(0.05, 0.2, 'Switch to pydata-sphinx-theme', **props)
+    t = fig.text(0.07, 0.2,
+                 '\nhttps://pydata-sphinx-theme.readthedocs.io/en/latest/',
+                 **props)
+    t.set_url('https://pydata-sphinx-theme.readthedocs.io/en/latest/')
 
     return fig
 
