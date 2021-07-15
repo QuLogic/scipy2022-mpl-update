c1a134650742fe1e8e4e29cbc8ba111041c52f97
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2021-07-15 03:08:42 -0400

    Add some miscellaneous 3.5 feature highlights.

--- feature35.py
+++ feature35.py
@@ -6,9 +6,10 @@ import io
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
 from PIL import Image
 
-from mplslide import FONT, new_slide, slide_heading, annotate_pr_author
+from mplslide import BULLET, FONT, new_slide, slide_heading, annotate_pr_author
 
 
 CODE = dict(fontfamily='monospace', fontsize=40, verticalalignment='top',
@@ -81,6 +82,52 @@ def cvdsim():
     return fig
 
 
+def misc():
+    """
+    Create slide for miscellaneous feature highlights.
+    """
+    fig = new_slide()
+    slide_heading(fig, '3.5 Miscellaneous Features')
+
+    props = dict(fontproperties=FONT, fontsize=48, alpha=0.7,
+                 verticalalignment='top')
+
+    fig.text(0.05, 0.7, 'New backends:', **props)
+    t = fig.text(0.05, 0.7, f'\n{BULLET} Qt6/PySide6', **props)
+    t.set_url('https://github.com/matplotlib/matplotlib/pull/19255')
+    t = fig.text(0.05, 0.7, f'\n\n{BULLET} GTK4', **props)
+    t.set_url('https://github.com/matplotlib/matplotlib/pull/20321')
+
+    fig.text(0.05, 0.5, 'Improved widgets:', **props)
+    t = fig.text(0.05, 0.5, f'\n{BULLET} Nicer Slider widget', **props)
+    t.set_url('https://github.com/matplotlib/matplotlib/pull/19265')
+    t = fig.text(0.05, 0.5, f'\n\n{BULLET} Draggable *Selector widgets',
+                 **props)
+    t.set_url('https://github.com/matplotlib/matplotlib/pull/19657')
+    t = fig.text(0.05, 0.5, f'\n\n\n{BULLET} Point removal in PolygonSelector',
+                 **props)
+    t.set_url('https://github.com/matplotlib/matplotlib/pull/19660')
+
+    # Simulate the old and new Sliders.
+    ax_old = fig.add_axes([0.4, 0.42, 0.5, 0.01])
+    ax_new = fig.add_axes([0.4, 0.4, 0.5, 0.01])
+    Slider(ax_new, "New", 0, 1)
+
+    valmin = 0
+    valinit = 0.5
+    ax_old.set_xlim([0, 1])
+    ax_old.axvspan(valmin, valinit, 0, 1)
+    ax_old.axvline(valinit, 0, 1, color="r", lw=1)
+    ax_old.set_xticks([])
+    ax_old.set_yticks([])
+    ax_old.text(-0.02, 0.5, "Old", transform=ax_old.transAxes,
+                verticalalignment="center", horizontalalignment="right")
+    ax_old.text(1.02, 0.5, "0.5", transform=ax_old.transAxes,
+                verticalalignment="center", horizontalalignment="left")
+
+    return fig
+
+
 def slides():
     """
     Return slides for this section.
@@ -88,4 +135,5 @@ def slides():
     return (
         legend_labelcolor(),
         cvdsim(),
+        misc(),
     )
