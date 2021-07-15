58434a6875d9528df330fd08fcd726e11a43a2f1
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2021-07-15 02:02:21 -0400

    Add a slide about the subplot_mosaic shortcut.

--- feature34.py
+++ feature34.py
@@ -55,10 +55,50 @@ for ax in axsRight:
     yield fig
 
 
+def identify_axes(ax_dict):
+    """
+    Mark Axes using their name passed to ``subplot_mosaic``.
+    """
+    kw = dict(ha='center', va='center', fontsize=48, color='darkgrey')
+    for k, ax in ax_dict.items():
+        ax.text(0.5, 0.5, k, transform=ax.transAxes, **kw)
+
+
+def mosaic():
+    """
+    Create slide for feature highlight of subplot_mosaic shortcut.
+    """
+
+    example1 = """
+    '''
+    AB
+    CC
+    '''"""
+    example2 = "'AB;CC'"
+
+    for text in [example1, example2]:
+        fig = new_slide()
+
+        slide_heading(fig, '3.4 Feature: subplot_mosaic shortcut')
+
+        fig.text(0.05, 0.8, f'plt.figure().subplot_mosaic({text})', **CODE)
+
+        ax_dict = fig.subplot_mosaic(eval(text.lstrip()),
+                                     # Don't overlay title and code.
+                                     gridspec_kw={'left': 0.3, 'top': 0.7,
+                                                  'right': 0.97})
+        identify_axes(ax_dict)
+
+        annotate_pr_author(fig, 'timhoffm', pr=18763)
+
+        yield fig
+
+
 def slides():
     """
     Return slides for this section.
     """
     return (
         *subfigure(),
+        *mosaic(),
     )
