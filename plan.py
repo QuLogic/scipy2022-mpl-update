8941b4977bf05e4d035bc8890b5e8331f7ba68ad
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2021-07-15 01:16:53 -0400

    Update future plans.

--- plan.py
+++ plan.py
@@ -16,13 +16,14 @@ def slides():
     props = dict(fontproperties=FONT, fontsize=56, alpha=0.7,
                  verticalalignment='top')
 
-    fig.text(0.05, 0.8, 'Next feature release: 3.4', **props)
-    fig.text(0.1, 0.7, f'{BULLET} September 2020', **props)
+    fig.text(0.05, 0.8, 'Next feature release: 3.5', **props)
+    fig.text(0.1, 0.7, f'{BULLET} September 2021', **props)
     fig.text(0.1, 0.6,
-             f'{BULLET} Dropping support for Python 3.6 & NumPy 1.15',
+             f'{BULLET} Dropping support for NumPy 1.16',
              **props)
-
-    fig.text(0.05, 0.4, 'Google Season of Docs 2020', **props)
+    fig.text(0.1, 0.5,
+             f'{BULLET} Adding PyPy wheels for all platforms',
+             **props)
 
     fig.text(0.05, 0.2, 'Check out our blog!', **props)
     t = fig.text(0.1, 0.2, '\nhttps://matplotlib.org/matplotblog/',
