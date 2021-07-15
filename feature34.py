beb4dfccd1dd59ed55516ff4d774dd209c2c99ee
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2021-07-15 02:09:57 -0400

    Add a slide about bar_label.

--- feature34.py
+++ feature34.py
@@ -2,6 +2,9 @@
 Feature highlights for Matplotlib 3.4.0.
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 from mplslide import FONT, new_slide, slide_heading, annotate_pr_author
 
 
@@ -94,6 +97,45 @@ def mosaic():
         yield fig
 
 
+def bar_label():
+    """
+    Create slide for feature highlight of bar_label.
+    """
+    fig = new_slide()
+    slide_heading(fig, '3.4 Feature: bar_label')
+
+    N = 5
+    men_means = (20, 35, 30, 35, -27)
+    women_means = (25, 32, 34, 20, -25)
+    men_std = (2, 3, 4, 1, 2)
+    women_std = (3, 5, 2, 3, 3)
+    ind = np.arange(N)    # the x locations for the groups
+    width = 0.35       # the width of the bars: can also be len(x) sequence
+
+    ax = fig.subplots()
+    fig.subplots_adjust(top=0.8)
+
+    p1 = ax.bar(ind, men_means, width, yerr=men_std, label='Men')
+    p2 = ax.bar(ind, women_means, width,
+                bottom=men_means, yerr=women_std, label='Women')
+
+    ax.axhline(0, color='grey', linewidth=0.8)
+    ax.set_ylabel('Scores', fontsize=20)
+    ax.set_xticks(ind)
+    ax.set_xticklabels(('G1', 'G2', 'G3', 'G4', 'G5'))
+    ax.tick_params(axis='both', labelsize=20)
+    ax.legend()
+
+    # Label with label_type 'center' instead of the default 'edge'
+    ax.bar_label(p1, label_type='center', fontsize=20)
+    ax.bar_label(p2, label_type='center', fontsize=20)
+    ax.bar_label(p2, fontsize=20)
+
+    annotate_pr_author(fig, 'immaxchen', pr=15602)
+
+    return fig
+
+
 def slides():
     """
     Return slides for this section.
@@ -101,4 +143,5 @@ def slides():
     return (
         *subfigure(),
         *mosaic(),
+        bar_label(),
     )
