1c32dd612f942a46224de191c4340b99759bf03b
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2022-07-14 14:54:00 -0500

    Update general news

--- news.py
+++ news.py
@@ -47,40 +47,47 @@ def bullet_level2(fig, y, text):
 
 def slides():
     """
-    Create slide for general news.
+    Create slide for general community news.
     """
     fig = new_slide()
 
-    slide_heading(fig, 'General News')
+    slide_heading(fig, 'Community News')
 
     level1 = partial(bullet_level1, fig)
     level2 = partial(bullet_level2, fig)
 
-    t = level1(0.8, f'{BULLET} Chan Zuckerberg Institute grant')
+    t = level1(0.8, f'{BULLET} Chan Zuckerberg Initiative grant')
     t.set_url('https://matplotlib.org/matplotblog/posts/matplotlib-rsef/')
-    level1(0.8, '\n    \N{EM dash} Essential Open Source Software for Science')
+    level1(0.8,
+           '\n    \N{EM dash} Essential Open Source Software for Science '
+           '(cycle 4)')
 
     level2(0.8,
-           '\n\nThomas Caswell, Hannah Aizenman,\nElliott Sales de Andrade')
+           '\n\nThomas Caswell, Elliott Sales de Andrade')
 
-    t = level1(0.525, f'{BULLET} Google Summer of Code')
+    t = level1(0.6, f'{BULLET} NASA ROSES-OSTFL 2020')
+    t.set_url('https://discourse.matplotlib.org/t/'
+              'maplotlib-selected-for-nasa-roses-ostfl-2020')
+    level1(0.6,
+           '\n    \N{EM dash} Revamping Matplotlib for Modern Data Structures')
+    level2(0.6, '\n\nKyle Sunden')
+
+    t = level1(0.4, f'{BULLET} Contributor Experience Leads')
     t.set_url(
-        'https://matplotlib.org/matplotblog/posts/gsoc_2021_introduction/')
+        'https://chanzuckerberg.com/eoss/proposals/'
+        'advancing-an-inclusive-culture-in-the-scientific-python-ecosystem/')
 
-    level2(0.525,
-           '\nAitik Gupta \N{EM dash} Revisiting Text/Font Handling')
+    level2(0.4, '\nMelissa Mendonça and Noa Tamir')
 
-    level1(0.375, f'{BULLET} Discourse')
+    level1(0.275,
+           f'{BULLET} Moved documentation from GitHub Pages to DigitalOcean')
 
-    t = level2(0.375, '\nhttps://discourse.matplotlib.org/')
-    t.set_url('https://discourse.matplotlib.org/')
+    level1(0.2, f'{BULLET} Third-party packages')
 
-    level1(0.225, f'{BULLET} Third-party packages')
-
-    t = level2(0.225, '\nPyPI classifier: Framework :: Matplotlib')
+    t = level2(0.2, '\nPyPI classifier: Framework :: Matplotlib')
     t.set_url('https://pypi.org/search/?c=Framework+%3A%3A+Matplotlib')
 
-    t = level2(0.225, '\n\nhttps://matplotlib.org/mpl-third-party/')
+    t = level2(0.2, '\n\nhttps://matplotlib.org/mpl-third-party/')
     t.set_url('https://matplotlib.org/mpl-third-party/')
 
     return fig
